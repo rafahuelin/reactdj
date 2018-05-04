732da0f6842f3a803abcadc776fb628181dce701
Rafa <rafahuelin@gmail.com>
2018-05-04 22:08:53 +0200

    add django-webpack-loader

--- src/djreact/settings.py
+++ src/djreact/settings.py
@@ -55,6 +55,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+    'webpack_loader',
 ]
 
 MIDDLEWARE = [
@@ -137,6 +140,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static_storage"),
+]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
+
 
 # Email host configuration from secrets.json
 
