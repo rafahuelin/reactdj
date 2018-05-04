083fef07ff8c81b640a859c56c893e3d655ab335
Rafa <rafahuelin@gmail.com>
2018-05-04 20:57:21 +0200

    add non ReactJS views

--- src/djreact/urls.py
+++ src/djreact/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('view2/', TemplateView.as_view(template_name='view2.html')),
+    path('view1/', TemplateView.as_view(template_name='view1.html')),
 ]
